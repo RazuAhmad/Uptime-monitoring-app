17e416a46222a25e9bcd2f0b305ffcade5aacd7e
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-05-30 20:34:59 +0600

    crud operation started

--- index.js
+++ index.js
@@ -8,20 +8,20 @@
 // dependencies:::::
 const http=require('http');
 const {handlerReqRes}=require('./helpers/handleReqRes');
+const environment =require('./helpers/environments')
 
 // app object - module scaffolding:::
 const app = {};
  
-// configuration::
-app.config={
-    port: 4000
-};
+
 
 // create server:::
 app.createServer = ()=>{
     const server =http.createServer(app.handleReqRes);
-    server.listen(app.config.port, ()=>{
-        console.log(`Listening to the port ${app.config.port}`);
+
+    server.listen(environment.port, ()=>{
+
+        console.log(`Listening to the port ${environment.port}`);
     })
 }
 
